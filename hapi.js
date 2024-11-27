c7983c1c80c6f21c1f6e58583b72bc8a8391716a
Yajan-G <yajangurung@gmail.com>
2024-11-27 12:42:30 +0545

    commit

---
+++ hapi.js
@@ -0,0 +1,14 @@
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+
+    const server = Hapi.server({
+        port: 3000,
+        host: 'localhost'
+    });
+
+    await server.start();
+    console.log('Server running on port 3000');
+};
+
+init();
